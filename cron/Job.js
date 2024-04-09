d4d67cd89964408d0bba03d048bf6fd234130035
tunntun <mervehtnozcan@gmail.com>
2024-04-10 01:25:29 +0300

    add comments

--- cron/Job.js
+++ cron/Job.js
@@ -9,16 +9,16 @@ const Job = {
         if (err)
           return console.log(err);
 
-        console.log(`Price updated ${Date.now()}`);
+        console.log(`Price updatesd ${Date.now()}`);
       });
     });
     croner.Cron(process.env.SCHEDULE || '*/5 * * * *' , () => {
-      Chain._updateChainPrices((err) => {
-        if (err)
-          return console.log(err);
-
-        console.log(`Price updated ${Date.now()}`);
-      });
+      // date'i kontrol et eğer date - current day > 60 dakika
+        //create snapshot with  is_hour: true
+      // date'i kontrol et eğer date - current day > 24 saat
+        //create snapshot with  is_day: true
+      // date'i kontrol et eğer date - current day > 30 gün
+        //create snapshot with  is_month: true
     });
     callback(null);
   }
